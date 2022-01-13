d4c246a40f7b77754323be577fea5663e56a6d94
lsunhash24 <lsanhueza2401@gmail.com>
2022-01-13 19:54:28 -0300

    main modificado

--- src/modelos/Main.java
+++ src/modelos/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Esto es una prueba para subir proyectos a GitHub");
+		System.out.println("Esto es una línea agregada");
 
 	}
 
